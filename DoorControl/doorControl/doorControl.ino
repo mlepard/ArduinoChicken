f777a3cce9127394197478bcf2fe8fc03c9bf0dd
Mike Lepard <mike.lepard@gmail.com>
2014-05-02 10:25:38 -0400

    alarm working now

--- DoorControl/doorControl/doorControl.ino
+++ DoorControl/doorControl/doorControl.ino
@@ -1,17 +1,16 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_PWMServoDriver.h"
-#include <SPI.h>
-#include <RTC_DS3231.h>
-#include <RTClib.h>
 #include <DS3231.h>
+#include <avr/sleep.h>
+#include <avr/power.h>
 
 const int motorNumber = 1;     
 const int forwardPin =  3;   
 const int reversePin = 5;
 const int alarmPin = 2;
 
-const int maxMotorTime = 5 * 1000; //max time for motor to operate in ms
+const int maxMotorTime = 2 * 1000; //max time for motor to operate in ms
 const int openDoorMotorDirection = FORWARD;
 const int closeDoorMotorDirection = BACKWARD;
 
@@ -21,16 +20,18 @@ const int motorSpeed = 25;
 #define ALRM1_MATCH_SEC        0b1110  // when seconds match
 #define ALRM1_MATCH_MIN_SEC    0b1100  // when minutes and seconds match
 #define ALRM1_MATCH_HR_MIN_SEC 0b1000  // when hours, minutes, and seconds match
-byte ALRM1_SET = ALRM1_MATCH_SEC;
+byte ALRM1_SET = ALRM1_MATCH_HR_MIN_SEC;
 
 #define ALRM2_ONCE_PER_MIN     0b111   // once per minute (00 seconds of every minute)
 #define ALRM2_MATCH_MIN        0b110   // when minutes match
 #define ALRM2_MATCH_HR_MIN     0b100   // when hours and minutes match
-byte ALRM2_SET = ALRM2_ONCE_PER_MIN;
+byte ALRM2_SET = ALRM2_MATCH_HR_MIN;
+
+byte AlarmBits;
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *doorMotor;
-RTC_DS3231 Clock;
+DS3231 Clock;
 
 bool tempDoorState = 0;
 
@@ -92,41 +93,82 @@ void closeDoor()
   doorMotor->run(RELEASE);
 }
 
+//Interrupt service routine for external interrupt on INT0 pin conntected to /INT
+void RTCAlarmTriggered()
+{
+  detachInterrupt(0);
+}
+
+void enterSleep(void)
+{
+  Serial.println("sleep!");
+  
+  
+  /* Setup pin2 as an interrupt and attach handler. */
+  attachInterrupt(0, RTCAlarmTriggered, LOW);
+  delay(100);
+  
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  
+  sleep_enable();
+  
+  sleep_mode();
+  
+  /* The program will continue from here. */
+  
+  /* First thing to do is disable sleep. */
+  sleep_disable(); 
+}
+
+void wakeUp()
+{
+  Clock.getTime(year, month, date, DoW, hour, minute, second);    
+  
+  Serial.print(hour, DEC);
+  Serial.print(':');
+  Serial.print(minute, DEC);
+  Serial.print(':');
+  Serial.print(second, DEC);
+  Serial.println();  
+}
+
 void setup() {
   Serial.begin(9600);
   
   pinMode(forwardPin, INPUT_PULLUP);      
   pinMode(reversePin, INPUT_PULLUP);
-  pinMode(alarmPin, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);      
+  pinMode(13, INPUT_PULLUP);      
     
   doorMotor = AFMS.getMotor(motorNumber);
   
+  Wire.begin();
   AFMS.begin();
   
-  doorMotor->setSpeed(motorSpeed); 
+  doorMotor->setSpeed(motorSpeed);
   
-  Wire.begin();
-  Clock.begin();
+  Serial.println("setup!");
 
-  DateTime now = Clock.now();  
+  Clock.getTime(year, month, date, DoW, hour, minute, second);    
   
+  Serial.print(hour, DEC);
+  Serial.print(':');
+  Serial.print(minute, DEC);
+  Serial.print(':');
+  Serial.print(second, DEC);
+  Serial.println();
+  
+  AlarmBits = ALRM2_SET;
+  AlarmBits <<= 4;
+  AlarmBits |= ALRM1_SET;
+  
+  Clock.enableOscillator(true,false, 0);
 
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.day(), DEC);
-    Serial.print(' ');
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
-    Serial.println();
+
 }
 
 void loop(){
-  int forwardState = digitalRead(forwardPin);
+  /*int forwardState = digitalRead(forwardPin);
   int reverseState = digitalRead(reversePin);
   
   // check if the pushbutton is pressed.
@@ -138,19 +180,20 @@ void loop(){
   else if (reverseState == LOW) {
     Serial.println("closeDoor");
    closeDoor();
-  }
+  }*/
   
-  /*  DateTime now = RTC.now();  
+  Clock.getTime(year, month, date, DoW, hour, minute, second);    
   
-  Serial.print("Current time is: ");
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(' ');
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.println(); */
+  
+  //Clock.setA1Time(DoW, hour, minute, second + 2, AlarmBits, true, false, false);
+  //Clock.setA2Time(Clock.getDate(), Clock.getHour(h12, PM), Clock.getMinute(), AlarmBits, false, false, false);
+  // Turn alarms on
+  //Clock.turnOnAlarm(1);
+  //Clock.turnOnAlarm(2);
+  
+  //Clock.enableOscillator(true,false, 0);
+
+  //enterSleep();
+  //wakeUp();  
+  
 }
